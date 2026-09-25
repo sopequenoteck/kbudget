828754af882c54c28d227b651a9718784d46bde5
sopequenoteck <sopequeno.tech@gmail.com>
2026-09-26 01:06:42 +0200

    fix(app): escapeHtml utilise replaceAll, signale par Sonar (KKS-376)

--- app/src/app/shared/utils/html-escape.utils.ts
+++ app/src/app/shared/utils/html-escape.utils.ts
@@ -7,9 +7,9 @@
  */
 export function escapeHtml(value: string): string {
   return value
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
 }
